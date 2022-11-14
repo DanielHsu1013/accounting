24aa985f4c1b1b838d13ca8db458573e88074483
Daniel Hsu <danielstark1013@gmail.com>
2022-11-14 20:41:55 +0800

    add new version

---
+++ accounting_4.py
@@ -0,0 +1,14 @@
+items = []
+while True:
+    name = input('Please enter item name:')
+    if name == 'q':
+        break
+    price = input('Please enter the price:')
+    items.append([name, price])
+
+for p in items:
+    print('the price of', p[0], 'is', p[1], 'dollers' )
+
+with open ('product.csv', 'w') as f:
+    for p in items:
+        f. write (p[0] + ',' + p[1] + '\n')
