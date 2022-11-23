a6345d10cf44b1cd0e344479a167f72e72ba86d2
Daniel Hsu <danielstark1013@gmail.com>
2022-11-23 18:28:15 +0800

    add new version to check if file exist

---
+++ accounting_8.py
@@ -0,0 +1,37 @@
+import os #operate system
+items = []
+
+#check if file exist
+if os.path.isfile('product.csv'):
+    print('the file is exist')
+
+    with open ('product.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '品項,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            items.append([name, price])  
+    print (items)
+
+else:
+    print('the file is not found')
+
+#user input
+items = []
+while True:
+    name = input('Please enter item name:')
+    if name == 'q':
+        break
+    price = input('Please enter the price:')
+    items.append([name, price])
+print(items)
+
+#print accounting record
+for p in items:
+    print('the price of', p[0], 'is', p[1], 'dollers' )
+
+#write the accounting record into csv file
+with open ('product.csv', 'w', encoding='utf-8') as f:
+    f.write('品項,價格\n' )
+    for p in items:
+        f. write (p[0] + ',' + p[1] + '\n')
